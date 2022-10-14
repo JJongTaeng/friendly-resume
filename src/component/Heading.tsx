7e249a98f34acf0168ae416988b2fe8e971e267d
intae <jjojjo141995@gmail.com>
2022-10-15 01:24:24 +0900

    Update Heading.tsx

--- src/component/Heading.tsx
+++ src/component/Heading.tsx
@@ -29,7 +29,7 @@ const Heading = () => {
       </Col>
       <Col>
         <Row gutter={[10, 20]}>
-          <Col {...{ xxl: 6, xl: 6, lg: 6, md: 8, sm: 12, xs: 24 }}>
+          <Col {...{ xxl: 6, xl: 6, lg: 6, md: 8, sm: 12, xs: -1 }}>
             <Image src={profile} alt='profile'/>
           </Col>
           <Col {...{ xxl: 18, xl: 18, lg: 18, md: 16, sm: 12, xs: 24 }}>
