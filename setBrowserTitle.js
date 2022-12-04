cad9962bead4b6ca02ae867e4136714a28d333f1
조인태 <63296903+JJongTaeng@users.noreply.github.com>
2022-12-04 11:02:58 +0900

    seo 최적화

--- setBrowserTitle.js
+++ setBrowserTitle.js
@@ -3,5 +3,9 @@ const fs = require('fs');
 const file = fs.readFileSync(__dirname + '/src/assets/resume.json');
 const json = JSON.parse(file.toString());
 const browserTitle = json.browserTitle;
+const meta = json.meta;
 
-fs.writeFileSync('.env', `REACT_APP_SITE_TITLE=${browserTitle}`);
+fs.writeFileSync('.env', `REACT_APP_SITE_TITLE=${browserTitle}\n`);
+if(meta) {
+  fs.appendFileSync('.env', `REACT_APP_META_TAG=${meta}`);
+}
